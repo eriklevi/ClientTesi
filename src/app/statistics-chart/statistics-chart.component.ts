3ab1c5741f2d28c028a870868fdb5770c6a1ed6b
eriklevi <levi.erik@gmail.com>
2019-05-24 15:26:34 +0200

    convertito stats in componente

---
+++ src/app/statistics-chart/statistics-chart.component.ts
@@ -0,0 +1,119 @@
+import { Component, OnInit } from '@angular/core';
+import {ChartOptions, ChartType} from 'chart.js';
+import {Label} from 'ng2-charts';
+import {AlertService} from '../_services/alert.service';
+import {CountedPacketsService} from '../_services/counted-packets.service';
+
+@Component({
+  selector: 'app-statistics-chart',
+  templateUrl: './statistics-chart.component.html',
+  styleUrls: ['./statistics-chart.component.css']
+})
+export class StatisticsChartComponent implements OnInit {
+
+
+  totalParsedPackets: number;
+  totalLocalParsedPackets: number;
+  totalGlobalParsedPackets: number;
+  globalPercent: number;
+  localPercent: number;
+  public loadingStats = false;
+  selectorValue = 'Total';
+  public pieChartOptions: ChartOptions = {
+    responsive: true,
+    legend: {
+      position: 'top',
+    },
+    plugins: {
+      datalabels: {
+        formatter: (value, ctx) => {
+          const label = ctx.chart.data.labels[ctx.dataIndex];
+          return label;
+        },
+      },
+    }
+  };
+  public pieChartLabels: Label[] = ['Local', 'Global'];
+  public pieChartData: number[];
+  public pieChartType: ChartType = 'pie';
+  public pieChartLegend = true;
+  public pieChartColors = [
+    {
+      backgroundColor: ['rgba(0,255,0,0.3)', 'rgba(0,0,255,0.3)'],
+    },
+  ];
+
+  constructor(
+    private alertService: AlertService,
+    private countedPacketsService: CountedPacketsService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  private fetchGeneralStats() {
+    this.loadingStats = true;
+    this.countedPacketsService.getTotalParsedPackets()
+      .subscribe(
+        data => {
+          this.totalLocalParsedPackets = data.local;
+          this.totalGlobalParsedPackets = data.global;
+          this.pieChartData = [data.local, data.global];
+          this.totalParsedPackets = data.local + data.global;
+          this.globalPercent = (data.global / (data.local + data.global)) * 100;
+          this.localPercent = (data.local / (data.local + data.global)) * 100;
+          this.loadingStats = false;
+        }, error1 => {
+          this.alertService.error('Impossible to retrieve packets statistics');
+          this.loadingStats = false;
+        }
+      );
+  }
+
+  fetchStatsOnChange() {
+    if (this.selectorValue === 'Total') {
+      this.fetchGeneralStatsOnChange();
+    } else {
+      this.fetchStatsBySnifferId(this.selectorValue);
+    }
+  }
+
+  private fetchStatsBySnifferId(selectorValue: string) {
+    this.loadingStats = true;
+    this.countedPacketsService.getTotalParsedPacketsBySnifferName(selectorValue)
+      .subscribe(
+        data => {
+          this.totalLocalParsedPackets = data.local;
+          this.totalGlobalParsedPackets = data.global;
+          this.totalParsedPackets = data.local + data.global;
+          this.globalPercent = (data.global / (data.local + data.global)) * 100;
+          this.localPercent = (data.local / (data.local + data.global)) * 100;
+          this.pieChartData = [data.local, data.global];
+          this.loadingStats = false;
+        },
+        error1 => {
+          this.alertService.error('Impossible to retrieve packet statistics');
+          this.loadingStats = false;
+        }
+      );
+  }
+
+  private fetchGeneralStatsOnChange() {
+    this.loadingStats = true;
+    this.countedPacketsService.getTotalParsedPackets()
+      .subscribe(
+        data => {
+          this.totalLocalParsedPackets = data.local;
+          this.totalGlobalParsedPackets = data.global;
+          this.totalParsedPackets = data.local + data.global;
+          this.globalPercent = (data.global / (data.local + data.global)) * 100;
+          this.localPercent = (data.local / (data.local + data.global)) * 100;
+          this.pieChartData = [data.local, data.global];
+          this.loadingStats = false;
+        }, error1 => {
+          this.alertService.error('Impossible to retrieve packets statistics');
+          this.loadingStats = false;
+        }
+      );
+  }
+}
